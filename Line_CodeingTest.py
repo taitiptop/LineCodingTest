47c2295d2341ce20598a86179c5db450efa94767
sigumza <39797124+sigumza@users.noreply.github.com>
2021-05-06 11:57:00 +0700

    Update Line_CodeingTest.py
    edit majority function

--- Line_CodeingTest.py
+++ Line_CodeingTest.py
@@ -1,3 +1,10 @@
+"""Majority element of an array is defined as a value x for 
+   which strictly more than half of array elements are equal to x.
+   Output the majority element of the given array a, or -1 if the array doesn't have majority element.
+   The first line of the input contains an integer N - the number of elements in the array. 1 <= N <= 100.
+   The second line of the input contains N integers - the elements of the array, separated with single spaces. 
+    1 <= ai <= 100."""
+
 def element_len():
     loop_status = None
     while loop_status != "pass":
@@ -5,11 +12,12 @@ def element_len():
             len_mem = int(input("Input number of element in array (1-100) :"))
         except :
             print("Error input")
-        if (len_mem>0) and len_mem<=100:
+        if (len_mem>0 & len_mem<=100):
             loop_status = "pass"
             return len_mem
         else :
             print("Error input")
+
 def element_mem(len_mem):
     loop_status = None
     while loop_status != "pass":
@@ -23,11 +31,17 @@ def element_mem(len_mem):
                 print(f"Error input")
         except :
             print("Error input")
+
 def majority(lst):
-    if set(lst) != len(lst):
-        reval =  max(set(lst), key = lst.count)
+    if len(set(lst)) != len(lst):
+        val_max =  max(set(lst), key = lst.count)
+        if lst.count(val_max) > len(lst)/2:
+            reval = val_max
+        else :
+            reval = -1
     else : 
         reval = -1
     print(f"Majority element of array is : {reval}")
+
 if __name__ == "__main__":
     majority(element_mem(element_len()))
